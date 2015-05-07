cd27d00df6024946444a52ce07c3e71b35aeb71d
Scot S <scot.scriven@gmail.com>
2015-05-07 21:22:28 +0100

    Create view for nutrient product

---
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/models/NutrientLevelItem.java
@@ -0,0 +1,7 @@
+package openfoodfacts.github.scrachx.openfood.models;
+
+/**
+ * Created by scotscriven on 07/05/15.
+ */
+public class NutrientLevelItem {
+}
---
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/adapters/NutrientLevelListAdapter.java
@@ -0,0 +1,7 @@
+package openfoodfacts.github.scrachx.openfood.views.adapters;
+
+/**
+ * Created by scotscriven on 07/05/15.
+ */
+public class NutrientLevelListAdapter {
+}
