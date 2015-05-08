c7cea7b425533cd3eb7f3ab4dcacd383105e8e9b
Scot S <scot.scriven@gmail.com>
2015-05-08 19:08:27 +0100

    Add splash screen and user view

---
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/UserFragment.java
@@ -0,0 +1,7 @@
+package openfoodfacts.github.scrachx.openfood.fragments;
+
+/**
+ * Created by scotscriven on 08/05/15.
+ */
+public class UserFragment {
+}
---
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/SplashActivity.java
@@ -0,0 +1,7 @@
+package openfoodfacts.github.scrachx.openfood.views;
+
+/**
+ * Created by scotscriven on 08/05/15.
+ */
+public class SplashActivity {
+}
