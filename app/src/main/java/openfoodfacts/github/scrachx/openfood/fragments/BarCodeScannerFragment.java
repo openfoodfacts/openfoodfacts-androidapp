30ec989a73430d5637ea120d90307f555abe007f
Scot Scriven <scot.scriven@gmail.com>
2016-04-24 10:54:15 +0200

    Update camera scan view

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/BarCodeScannerFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/BarCodeScannerFragment.java
@@ -1,48 +1,159 @@
 package openfoodfacts.github.scrachx.openfood.fragments;
 
-import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.view.MenuItemCompat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.Result;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import openfoodfacts.github.scrachx.openfood.R;
 import openfoodfacts.github.scrachx.openfood.models.FoodAPIRestClientUsage;
-import openfoodfacts.github.scrachx.openfood.views.MainActivity;
 
-public class BarCodeScannerFragment extends BaseFragment implements ZXingScannerView.ResultHandler {
+public class BarCodeScannerFragment extends BaseFragment implements MessageDialogFragment.MessageDialogListener,
+        ZXingScannerView.ResultHandler, CameraSelectorDialogFragment.CameraSelectorDialogListener {
 
+    private static final String FLASH_STATE = "FLASH_STATE";
+    private static final String AUTO_FOCUS_STATE = "AUTO_FOCUS_STATE";
+    private static final String CAMERA_ID = "CAMERA_ID";
     private ZXingScannerView mScannerView;
+    private boolean mFlash;
+    private boolean mAutoFocus;
+    private int mCameraId = -1;
     private List<BarcodeFormat> mFormats;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
         mScannerView = new ZXingScannerView(getActivity());
-        mScannerView.setFocusableInTouchMode(true);
-        mScannerView.requestFocus();
-        mScannerView.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
-                    Intent intent = new Intent(getActivity(), MainActivity.class);
-                    startActivity(intent);
-                    getActivity().finish();
-                    return true;
+        if(state != null) {
+            mFlash = state.getBoolean(FLASH_STATE, false);
+            mAutoFocus = state.getBoolean(AUTO_FOCUS_STATE, true);
+            mCameraId = state.getInt(CAMERA_ID, -1);
+        } else {
+            mFlash = false;
+            mAutoFocus = true;
+            mCameraId = -1;
+        }
+        setupFormats();
+        return mScannerView;
+    }
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setHasOptionsMenu(true);
+    }
+
+    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        MenuItem menuItem;
+
+        if(mFlash) {
+            menuItem = menu.add(Menu.NONE, R.id.menu_flash, 0, R.string.flash_on);
+        } else {
+            menuItem = menu.add(Menu.NONE, R.id.menu_flash, 0, R.string.flash_off);
+        }
+        MenuItemCompat.setShowAsAction(menuItem, MenuItem.SHOW_AS_ACTION_NEVER);
+
+
+        if(mAutoFocus) {
+            menuItem = menu.add(Menu.NONE, R.id.menu_auto_focus, 0, R.string.auto_focus_on);
+        } else {
+            menuItem = menu.add(Menu.NONE, R.id.menu_auto_focus, 0, R.string.auto_focus_off);
+        }
+        MenuItemCompat.setShowAsAction(menuItem, MenuItem.SHOW_AS_ACTION_NEVER);
+
+        menuItem = menu.add(Menu.NONE, R.id.menu_camera_selector, 0, R.string.select_camera);
+        MenuItemCompat.setShowAsAction(menuItem, MenuItem.SHOW_AS_ACTION_NEVER);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.menu_flash:
+                mFlash = !mFlash;
+                if(mFlash) {
+                    item.setTitle(R.string.flash_on);
                 } else {
-                    return false;
+                    item.setTitle(R.string.flash_off);
                 }
-            }
-        });
+                mScannerView.setFlash(mFlash);
+                return true;
+            case R.id.menu_auto_focus:
+                mAutoFocus = !mAutoFocus;
+                if(mAutoFocus) {
+                    item.setTitle(R.string.auto_focus_on);
+                } else {
+                    item.setTitle(R.string.auto_focus_off);
+                }
+                mScannerView.setAutoFocus(mAutoFocus);
+                return true;
+            case R.id.menu_camera_selector:
+                mScannerView.stopCamera();
+                DialogFragment cFragment = CameraSelectorDialogFragment.newInstance(this, mCameraId);
+                cFragment.show(getActivity().getSupportFragmentManager(), "camera_selector");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera(mCameraId);
+        mScannerView.setFlash(mFlash);
+        mScannerView.setAutoFocus(mAutoFocus);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(FLASH_STATE, mFlash);
+        outState.putBoolean(AUTO_FOCUS_STATE, mAutoFocus);
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getActivity().getApplicationContext(), notification);
+            r.play();
+        } catch (Exception e) {}
+        if (!rawResult.getText().isEmpty()) {
+            goToProduct(rawResult, mScannerView);
+        }
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // Resume the camera
+        mScannerView.resumeCameraPreview(this);
+    }
+
+    @Override
+    public void onCameraSelected(int cameraId) {
+        mCameraId = cameraId;
+        mScannerView.startCamera(mCameraId);
+        mScannerView.setFlash(mFlash);
+        mScannerView.setAutoFocus(mAutoFocus);
+    }
+
+    public void setupFormats() {
         mFormats = new ArrayList<>();
         mFormats.add(BarcodeFormat.UPC_A);
         mFormats.add(BarcodeFormat.UPC_E);
@@ -54,43 +165,16 @@ public class BarCodeScannerFragment extends BaseFragment implements ZXingScanner
         mFormats.add(BarcodeFormat.CODE_128);
         mFormats.add(BarcodeFormat.ITF);
         mScannerView.setFormats(mFormats);
-        mScannerView.setResultHandler(this);
-        mScannerView.setAutoFocus(true);
-        return mScannerView;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mScannerView.setFormats(mFormats);
-        mScannerView.setResultHandler(this);
-        mScannerView.setAutoFocus(true);
-        mScannerView.startCamera();
-    }
-
-    @Override
-    public void handleResult(Result rawResult) {
-        try {
-            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone r = RingtoneManager.getRingtone(getActivity().getApplicationContext(), notification);
-            r.play();
-        } catch (Exception e) {
-        }
-        if (rawResult.getText().isEmpty()) {
-
-        } else {
-            goToProduct(rawResult, mScannerView);
-        }
+    private void goToProduct(Result rawResult, ZXingScannerView scannerView) {
+        FoodAPIRestClientUsage api = new FoodAPIRestClientUsage();
+        api.getProduct(rawResult.getText(), getActivity(), scannerView);
     }
 
     @Override
     public void onPause() {
         super.onPause();
         mScannerView.stopCamera();
-    }
-
-    private void goToProduct(Result rawResult, ZXingScannerView scannerView) {
-        FoodAPIRestClientUsage api = new FoodAPIRestClientUsage();
-        api.getProduct(rawResult.getText(), getActivity(), scannerView);
     }
 }
--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ScannerFragmentActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ScannerFragmentActivity.java
@@ -4,25 +4,13 @@ import android.os.Bundle;
 
 import openfoodfacts.github.scrachx.openfood.R;
 
-public class ScannerFragmentActivity extends BaseActivity {
+public class ScannerFragmentActivity extends BaseScannerActivity {
+
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
         setContentView(R.layout.fragment_scanner);
+        setupToolbar();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 }
