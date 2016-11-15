c8abb24a6618e40352dfa1676cad5268958ba085
Aurélien Leboulanger <aurelien.leboulanger@3ds.com>
2016-11-15 16:09:40 +0100

    add languages english labels for the preference list fix 177 recreate the activity when a preference is changed fix 227 add some french translations externalize strings for the preferences panel

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/PreferencesFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/PreferencesFragment.java
@@ -1,7 +1,14 @@
 package openfoodfacts.github.scrachx.openfood.fragments;
 
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+
+import java.util.Locale;
+
 import openfoodfacts.github.scrachx.openfood.R;
 
 public class PreferencesFragment extends PreferenceFragmentCompat {
@@ -9,5 +16,18 @@ public class PreferencesFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        findPreference("Locale.Helper.Selected.Language").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object locale) {
+                FragmentActivity activity = PreferencesFragment.this.getActivity();
+                Configuration configuration = activity.getResources().getConfiguration();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    configuration.setLocale(new Locale(((String) locale)));
+                    activity.recreate();
+                }
+                return true;
+            }
+        });
     }
 }
