40d1d84d7a22806112c94a3d83ab32214fb5659a
Aurélien Leboulanger <aurelien.leboulanger@3ds.com>
2016-12-19 13:57:49 +0100

    Add label values in the summary view of the product fix #245

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/utils/Utils.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/utils/Utils.java
@@ -5,8 +5,13 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -22,6 +27,60 @@ public class Utils {
 
     public static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
     public static final int MY_PERMISSIONS_REQUEST_STORAGE= 2;
+
+    /**
+     * Returns a CharSequence that concatenates the specified array of CharSequence
+     * objects and then applies a list of zero or more tags to the entire range.
+     *
+     * @param content an array of character sequences to apply a style to
+     * @param tags the styled span objects to apply to the content
+     *        such as android.text.style.StyleSpan
+     *
+     */
+    private static CharSequence apply(CharSequence[] content, Object... tags) {
+        SpannableStringBuilder text = new SpannableStringBuilder();
+        openTags(text, tags);
+        for (CharSequence item : content) {
+            text.append(item);
+        }
+        closeTags(text, tags);
+        return text;
+    }
+
+    /**
+     * Iterates over an array of tags and applies them to the beginning of the specified
+     * Spannable object so that future text appended to the text will have the styling
+     * applied to it. Do not call this method directly.
+     */
+    private static void openTags(Spannable text, Object[] tags) {
+        for (Object tag : tags) {
+            text.setSpan(tag, 0, 0, Spannable.SPAN_MARK_MARK);
+        }
+    }
+
+    /**
+     * "Closes" the specified tags on a Spannable by updating the spans to be
+     * endpoint-exclusive so that future text appended to the end will not take
+     * on the same styling. Do not call this method directly.
+     */
+    private static void closeTags(Spannable text, Object[] tags) {
+        int len = text.length();
+        for (Object tag : tags) {
+            if (len > 0) {
+                text.setSpan(tag, 0, len, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            } else {
+                text.removeSpan(tag);
+            }
+        }
+    }
+
+    /**
+     * Returns a CharSequence that applies boldface to the concatenation
+     * of the specified CharSequence objects.
+     */
+    public static CharSequence bold(CharSequence... content) {
+        return apply(content, new StyleSpan(Typeface.BOLD));
+    }
 
     public static void hideKeyboard(Activity activity) {
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
