a445bcafafe612d4e71f5fcf83b9bae5a4cf58e8
Aurélien Leboulanger <aurelien.leboulanger@3ds.com>
2017-01-06 16:20:35 +0100

    use recyclerView instead of listview

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/SearchProductsResultsFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/SearchProductsResultsFragment.java
@@ -2,25 +2,27 @@ package openfoodfacts.github.scrachx.openfood.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.OnItemClick;
 import openfoodfacts.github.scrachx.openfood.R;
 import openfoodfacts.github.scrachx.openfood.models.Product;
 import openfoodfacts.github.scrachx.openfood.network.OpenFoodAPIClient;
-import openfoodfacts.github.scrachx.openfood.views.adapters.ProductsListAdapter;
+import openfoodfacts.github.scrachx.openfood.views.adapters.ProductsRecyclerViewAdapter;
 
 public class SearchProductsResultsFragment extends BaseFragment {
 
     private OpenFoodAPIClient api;
 
-    @BindView(R.id.listProducts) ListView listView;
+    private RecyclerView productsRecyclerView;
+
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
@@ -33,24 +35,39 @@ public class SearchProductsResultsFragment extends BaseFragment {
     public void onViewCreated(View view, @Nullable Bundle state) {
         super.onViewCreated(view, state);
 
+        productsRecyclerView = (RecyclerView) view.findViewById(R.id.products_recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        productsRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        productsRecyclerView.setLayoutManager(mLayoutManager);
+
+        // use VERTICAL divider
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(productsRecyclerView.getContext(),
+                DividerItemDecoration.VERTICAL);
+        productsRecyclerView.addItemDecoration(dividerItemDecoration);
+
         String query = this.getArguments().getString("query");
 
         api.searchProduct(query, getActivity(),
                 new OpenFoodAPIClient.OnProductsCallback() {
+
                     @Override
                     public void onProductsResponse(boolean isResponseOk, List<Product> products) {
                         if (isResponseOk) {
-                            ProductsListAdapter adapter = new ProductsListAdapter(getActivity(), products);
-                            listView.setAdapter(adapter);
+                            RecyclerView.Adapter adapter = new ProductsRecyclerViewAdapter(products);
+                            productsRecyclerView.setAdapter(adapter);
                         }
                     }
                 }
         );
     }
 
-    @OnItemClick(R.id.listProducts)
+//    @OnItemClick(R.id.listProducts)
     protected void onProductClicked(int position) {
-        Product p = (Product) listView.getItemAtPosition(position);
+        Product p = ((ProductsRecyclerViewAdapter) productsRecyclerView.getAdapter()).getProduct(position);
         String barcode = p.getCode();
         api.getProduct(barcode, getActivity());
     }
