4e5170af7e14fa98149a3cf9ead372a912248edd
Pierre Slamich <pierreslam@Slambook.local>
2018-01-03 00:04:38 +0100

    Revert "Merge pull request #610 from ChasakisD/private-rating"
    This reverts commit e63313e5d674fa52b6fce9e3847230c9c677f166, reversing
    changes made to b67b9eac09b497543758951a69b51618e0284b0f.

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/models/DatabaseHelper.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/models/DatabaseHelper.java
@@ -5,7 +5,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import org.greenrobot.greendao.database.Database;
-import org.greenrobot.greendao.database.StandardDatabase;
 
 public class DatabaseHelper extends DaoMaster.OpenHelper {
 
@@ -43,9 +42,6 @@ public class DatabaseHelper extends DaoMaster.OpenHelper {
         switch (migrateVersion) {
             case 2:
                 db.execSQL("ALTER TABLE send_product ADD COLUMN 'lang' TEXT NOT NULL DEFAULT 'fr';");
-                break;
-            case 3:
-                RatingProductDao.createTable(new StandardDatabase(db), false);
                 break;
         }
     }
