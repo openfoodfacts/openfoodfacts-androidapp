27e476abdf0fa86a89e2ee7cd3aefe9a090f26f3
policorn <18316436+policorn@users.noreply.github.com>
2018-02-12 00:28:15 +0100

    Issue 698 - Language codes appear as translatable: main config and fragment code

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/PreferencesFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/PreferencesFragment.java
@@ -4,7 +4,12 @@ import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+
+import org.apache.commons.text.WordUtils;
+
+import java.util.Locale;
 
 import openfoodfacts.github.scrachx.openfood.R;
 import openfoodfacts.github.scrachx.openfood.utils.LocaleHelper;
@@ -14,8 +19,26 @@ public class PreferencesFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+        ListPreference languagePreference = ((ListPreference) findPreference("Locale.Helper.Selected.Language"));
 
-        findPreference("Locale.Helper.Selected.Language").setOnPreferenceChangeListener((preference, locale) -> {
+        String[] localeValues = getActivity().getResources().getStringArray(R.array.lang_array);
+        String[] localeLabels = new String[localeValues.length];
+
+        for (int i = 0; i < localeValues.length; i++) {
+            Locale current = LocaleHelper.getLocale(localeValues[i]);
+
+            localeLabels[i] = String.format("%s - %s",
+                    // current.getDisplayName(current), // native form
+                    WordUtils.capitalize(current.getDisplayName()),
+                    localeValues[i].toUpperCase()
+                    );
+        }
+
+        languagePreference.setEntries(localeLabels);
+        languagePreference.setEntryValues(localeValues);
+
+        languagePreference.setOnPreferenceChangeListener((preference, locale) -> {
+
             FragmentActivity activity = PreferencesFragment.this.getActivity();
             Configuration configuration = activity.getResources().getConfiguration();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
