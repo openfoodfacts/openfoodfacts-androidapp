b12f43df82c8bc45b83600d51962e1348772e3fb
Shubham Vishwakarma <shubhamvishwakarma2001@gmail.com>
2018-02-14 19:36:07 +0530

    fix #468 Add spaces after commas in Product View

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/SearchProductsResultsFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/SearchProductsResultsFragment.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
