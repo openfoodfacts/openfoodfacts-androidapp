25b542ebadfda6c5cde6c9523e549342424946cd
absolutelysaurabh <absolutelysaurabh03@gmail.com>
2018-02-17 04:42:31 +0530

    fix: toast on back pressed in home activity

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/BaseActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/BaseActivity.java
@@ -1,11 +1,14 @@
 package openfoodfacts.github.scrachx.openfood.views;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
-
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import openfoodfacts.github.scrachx.openfood.dagger.component.ActivityComponent;
 import openfoodfacts.github.scrachx.openfood.dagger.module.ActivityModule;
@@ -14,7 +17,7 @@ import openfoodfacts.github.scrachx.openfood.utils.LocaleHelper;
 public abstract class BaseActivity extends AppCompatActivity {
 
     private ActivityComponent activityComponent;
-
+    private boolean doubleBackToExitPressedOnce;
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
@@ -35,5 +38,24 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     public ActivityComponent getActivityComponent() {
         return activityComponent;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please press back again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
