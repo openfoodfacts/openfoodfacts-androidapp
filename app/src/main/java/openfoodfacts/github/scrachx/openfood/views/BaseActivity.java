02c3cd4ebfea8c3ce0ac39ddc27453f5764629f3
absolutelysaurabh <absolutelysaurabh03@gmail.com>
2018-02-20 20:36:05 +0530

    added_toast_in_exit

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/BaseActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/BaseActivity.java
@@ -1,12 +1,15 @@
 package openfoodfacts.github.scrachx.openfood.views;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
+import openfoodfacts.github.scrachx.openfood.R;
 import openfoodfacts.github.scrachx.openfood.dagger.component.ActivityComponent;
 import openfoodfacts.github.scrachx.openfood.dagger.module.ActivityModule;
 import openfoodfacts.github.scrachx.openfood.utils.LocaleHelper;
@@ -14,7 +17,7 @@ import openfoodfacts.github.scrachx.openfood.utils.LocaleHelper;
 public abstract class BaseActivity extends AppCompatActivity {
 
     private ActivityComponent activityComponent;
-
+    private boolean doubleBackToExitPressedOnce;
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
@@ -35,5 +38,24 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     public ActivityComponent getActivityComponent() {
         return activityComponent;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, this.getString(R.string.app_exit_toast_message), Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
