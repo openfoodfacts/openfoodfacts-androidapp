c8a96e10b86023df654ac45e1562833731f35b7c
PRAJWAL M <30733262+PrajwalM2212@users.noreply.github.com>
2018-02-25 23:08:49 +0530

    Add a way to contact the team (#911)

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/PreferencesFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/PreferencesFragment.java
@@ -1,11 +1,15 @@
 package openfoodfacts.github.scrachx.openfood.fragments;
 
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.widget.Toast;
 
 import org.apache.commons.text.WordUtils;
 
@@ -48,6 +52,24 @@ public class PreferencesFragment extends PreferenceFragmentCompat {
                 activity.recreate();
             }
             return true;
+        });
+
+        Preference contactButton = findPreference(getString(R.string.contact_key));
+        contactButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                Intent contactIntent = new Intent(Intent.ACTION_SENDTO);
+                contactIntent.setData(Uri.parse(getString(R.string.off_mail)));
+                contactIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                try{
+                    startActivity(contactIntent);
+                }catch (android.content.ActivityNotFoundException e){
+
+                    Toast.makeText(getActivity(), R.string.email_not_found,Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
         });
     }
 }
