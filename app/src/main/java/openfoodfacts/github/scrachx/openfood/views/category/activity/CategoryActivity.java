76db4daefcd70fb596807e4c9a719c4bc111b387
Samagra <samagra14@gmail.com>
2018-02-28 22:26:17 +0530

    Adds connectivity check on categories

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/category/activity/CategoryActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/category/activity/CategoryActivity.java
@@ -2,6 +2,8 @@ package openfoodfacts.github.scrachx.openfood.views.category.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import openfoodfacts.github.scrachx.openfood.R;
@@ -16,10 +18,28 @@ public class CategoryActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
+        checkNetworkAndShowScreen();
+    }
 
-        setSupportActionBar(findViewById(R.id.toolbar));
-        setTitle(R.string.category_drawer);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    private void checkNetworkAndShowScreen(){
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        if (isConnected) {
+
+            setContentView(R.layout.activity_category);
+
+            setSupportActionBar(findViewById(R.id.toolbar));
+            setTitle(R.string.category_drawer);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        else{
+            setContentView(R.layout.offline_cloud_layout);
+            findViewById(R.id.buttonToRefresh).setOnClickListener(view -> checkNetworkAndShowScreen());
+
+        }
     }
 }
