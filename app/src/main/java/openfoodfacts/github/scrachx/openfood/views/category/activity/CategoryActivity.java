3427c49bfe55643abc696dfc7080c0321dead1bb
Samagra <samagra14@gmail.com>
2018-03-01 14:07:56 +0530

    final_corrections

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/category/activity/CategoryActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/category/activity/CategoryActivity.java
@@ -3,12 +3,15 @@ package openfoodfacts.github.scrachx.openfood.views.category.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import openfoodfacts.github.scrachx.openfood.R;
 import openfoodfacts.github.scrachx.openfood.utils.Utils;
 import openfoodfacts.github.scrachx.openfood.views.BaseActivity;
 
 public class CategoryActivity extends BaseActivity {
+
+    private View offlineView , onlineView;
 
     public static Intent getIntent(Context context) {
         return new Intent(context, CategoryActivity.class);
@@ -17,19 +20,26 @@ public class CategoryActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+        setSupportActionBar(findViewById(R.id.toolbar));
+        setTitle(R.string.category_drawer);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        findViewById(R.id.buttonToRefresh).setOnClickListener(view -> checkNetworkAndShowScreen());
+        offlineView = findViewById(R.id.offline_view);
+        onlineView = findViewById(R.id.fragment);
         checkNetworkAndShowScreen();
+
     }
 
     private void checkNetworkAndShowScreen(){
         if (Utils.isNetworkConnected(this)) {
-            setContentView(R.layout.activity_category);
-            setSupportActionBar(findViewById(R.id.toolbar));
-            setTitle(R.string.category_drawer);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            onlineView.setVisibility(View.VISIBLE);
+            offlineView.setVisibility(View.GONE);
         }
         else{
-            setContentView(R.layout.offline_cloud_layout);
-            findViewById(R.id.buttonToRefresh).setOnClickListener(view -> checkNetworkAndShowScreen());
+            offlineView.setVisibility(View.VISIBLE);
+            onlineView.setVisibility(View.GONE);
+
         }
     }
 }
