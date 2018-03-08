ae2ae6f1ecf54434d133410ce8d076628eb22c4a
ManavPoddar <manavpoddar17@gmail.com>
2018-03-08 08:55:25 +0530

    Fixes #1146 Loading Toast keeps loading in Allergen Alert

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/AlertUserFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/AlertUserFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import net.steamcrafted.loadtoast.LoadToast;
@@ -149,6 +151,12 @@ public class AlertUserFragment extends BaseFragment {
                                 lt.success();
                                 dialog.hide();
                             });
+                        })
+                        .onNegative(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                lt.hide();
+                            }
                         })
                         .show();
             } else {
