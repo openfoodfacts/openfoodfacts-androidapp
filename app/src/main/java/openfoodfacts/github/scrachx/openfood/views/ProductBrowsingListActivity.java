67e9e380271f8b78990aac232d3534b251d0ad53
jayanth reddy <sjayanthreddy2@gmail.com>
2018-03-20 18:29:34 +0530

    Update ProductBrowsingListActivity.java

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ProductBrowsingListActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ProductBrowsingListActivity.java
@@ -129,7 +129,6 @@ public class ProductBrowsingListActivity extends BaseActivity {
 
         searchType = extras.getString(SEARCH_TYPE);
         key = extras.getString(KEY);
-        
         newSearchQuery();
     }
 
