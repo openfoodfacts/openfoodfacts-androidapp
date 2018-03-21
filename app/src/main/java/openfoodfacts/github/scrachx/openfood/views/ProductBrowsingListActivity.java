ef6b7ddf732304e474a5ac26d6b8e12d89a62603
Amanjeet Singh <amanjeetsingh150@gmail.com>
2018-03-21 14:47:44 +0530

    Closes #1275: Vitamins, minerals, amino acid and others tags support added (#1282)
    * feat: fixed the Load Toast visibility
    
    * feat: added support for vitamin,mineral,etc.
    
    * feat: trimmed language part
    
    * feat: added commas and documented the function
    
    * feat: updated the strings for tags

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ProductBrowsingListActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ProductBrowsingListActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
