fd0a30fd2840e6d3fa168fc7c5c54f09b7cbb94c
Louis-Gabriel ZAITI <lgzaiti@unowhy.com>
2018-03-29 01:42:19 +0200

    Add Fastlane configuration files and dependencies

---
+++ app/src/androidTest/java/JUnit4StyleTests.java
@@ -0,0 +1,49 @@
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import openfoodfacts.github.scrachx.openfood.R;
+import openfoodfacts.github.scrachx.openfood.views.MainActivity;
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * <p>Created by lgzaiti on 27/03/18 <br/>
+ */
+@RunWith(JUnit4.class)
+public class JUnit4StyleTests {
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @BeforeClass
+    public static void beforeAll() {
+        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
+    }
+
+    @Test
+    public void testTakeScreenshot() {
+        onView(withId(R.id.action_search)).check(matches(isDisplayed()));
+
+        Screengrab.screenshot("beforeFabClick");
+
+        onView(withId(R.id.action_search)).perform(click());
+
+        Screengrab.screenshot("afterFabClick");
+    }
+
+}
