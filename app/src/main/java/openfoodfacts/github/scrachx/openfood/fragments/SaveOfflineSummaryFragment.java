f358a9891106e97c8a2bcddf2778ffa8cf934c98
jayanth reddy <sjayanthreddy2@gmail.com>
2018-04-04 14:23:36 +0530

    Update SaveOfflineSummaryFragment.java

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/SaveOfflineSummaryFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/SaveOfflineSummaryFragment.java
@@ -168,7 +168,6 @@ public class SaveOfflineSummaryFragment extends BaseFragment {
         } catch (NullPointerException e) {
             if (BuildConfig.DEBUG) Log.i(getClass().getSimpleName(), e.toString());
         }
-
         mProduct.setLang(Locale.getDefault().getLanguage());
     }
 
