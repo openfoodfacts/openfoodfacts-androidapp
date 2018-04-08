af0669839c3a5b317e1d9fefea2b4e820ecdc8b0
jayanth reddy <sjayanthreddy2@gmail.com>
2018-04-08 18:58:04 +0530

    Update NutritionInfoProductFragment.java

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/NutritionInfoProductFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/NutritionInfoProductFragment.java
@@ -113,6 +113,7 @@ public class NutritionInfoProductFragment extends BaseFragment {
             mUrlImage = product.getImageNutritionUrl();
         }
 
+
         if (nutriments == null) {
             return;
         }
