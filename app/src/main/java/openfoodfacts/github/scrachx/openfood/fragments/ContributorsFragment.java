d3ff39329342e25360141d29bd9fea4a58030cfa
PrajwalM2212 <prajwalmmath@gmail.com>
2018-04-14 13:15:13 +0530

    Clickabel

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/ContributorsFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/ContributorsFragment.java
@@ -3,10 +3,15 @@ package openfoodfacts.github.scrachx.openfood.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,12 +19,18 @@ import java.util.List;
 
 import butterknife.BindView;
 import openfoodfacts.github.scrachx.openfood.R;
+import openfoodfacts.github.scrachx.openfood.models.CategoryName;
 import openfoodfacts.github.scrachx.openfood.models.Product;
 import openfoodfacts.github.scrachx.openfood.models.SendProduct;
 import openfoodfacts.github.scrachx.openfood.models.State;
 import openfoodfacts.github.scrachx.openfood.network.OpenFoodAPIClient;
 import openfoodfacts.github.scrachx.openfood.network.WikidataApiClient;
+import openfoodfacts.github.scrachx.openfood.utils.SearchType;
 import openfoodfacts.github.scrachx.openfood.utils.Utils;
+import openfoodfacts.github.scrachx.openfood.views.ProductActivity;
+import openfoodfacts.github.scrachx.openfood.views.ProductBrowsingListActivity;
+
+import static android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE;
 
 /**
  * Created by prajwalm on 14/04/18.
@@ -56,11 +67,15 @@ public class ContributorsFragment extends BaseFragment {
             String[] lastEditDate = getDateTime(product.getLastModifiedTime());
             String editors = getEditors(product.getEditors(), product.getCreator(), product.getLastModifiedBy());
 
-            String creatorTxt = getString(R.string.creator_history, createdDate[0], createdDate[1], product.getCreator());
+
+            //String creatorTxt = getContributorsTag(product.getCreator()).toString();
             String editorTxt = getString(R.string.last_editor_history, lastEditDate[0], lastEditDate[1], product.getLastModifiedBy());
             String otherEditorsTxt = getString(R.string.other_editors, editors);
 
-            creatorText.setText(creatorTxt);
+            creatorText.setClickable(true);
+            creatorText.setMovementMethod(LinkMovementMethod.getInstance());
+            creatorText.setText("Creator");
+            creatorText.append(getContributorsTag(product.getCreator()));
             lastEditorText.setText(editorTxt);
             otherEditorsText.setText(otherEditorsTxt);
         }
@@ -111,6 +126,22 @@ public class ContributorsFragment extends BaseFragment {
 
         return builder.toString();
 
+    }
+
+    private CharSequence getContributorsTag(String contributor) {
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+
+                ProductBrowsingListActivity.startActivity(getContext(), contributor, SearchType.CONTRIBUTOR);
+
+            }
+        };
+        spannableStringBuilder.append(contributor);
+        spannableStringBuilder.setSpan(clickableSpan, 0, spannableStringBuilder.length(), SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableStringBuilder.append(" ");
+        return spannableStringBuilder;
     }
 
 }
