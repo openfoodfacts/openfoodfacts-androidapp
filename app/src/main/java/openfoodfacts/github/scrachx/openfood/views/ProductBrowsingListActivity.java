b0d3744dc6c2e2c245ba8d1bda93f5637aef5960
PrajwalM2212 <prajwalmmath@gmail.com>
2018-04-17 18:11:10 +0530

    minor fix

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ProductBrowsingListActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ProductBrowsingListActivity.java
@@ -217,19 +217,23 @@ public class ProductBrowsingListActivity extends BaseActivity {
             case SearchType.CONTRIBUTOR:
                 getSupportActionBar().setSubtitle(getString(R.string.contributor_string));
                 break;
+            case SearchType.INCOMPLETE_PRODUCT: {
+                getSupportActionBar().setTitle(getString(R.string.products_to_be_completed));
+                break;
+            }
 
             case SearchType.STATE: {
                 getSupportActionBar().setSubtitle("State");
                 break;
             }
-            case SearchType.INCOMPLETE_PRODUCT: {
-                getSupportActionBar().setTitle(getString(R.string.products_to_be_completed));
-                break;
-            }
-            default: {
+
+            default:
+
                 Log.e("Products Browsing", "No math case found for " + searchType);
-            }
+
+
         }
+
 
         apiClient = new OpenFoodAPIClient(ProductBrowsingListActivity.this, BuildConfig.OFWEBSITE);
         api = new OpenFoodAPIClient(ProductBrowsingListActivity.this);
