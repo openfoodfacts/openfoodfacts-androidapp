e6fdb4fde1b3d29b10d95d52cad3c850d485bb15
PrajwalM2212 <prajwalmmath@gmail.com>
2018-04-19 00:00:44 +0530

    changes

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/MainActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/MainActivity.java
@@ -408,6 +408,7 @@ public class MainActivity extends BaseActivity implements CustomTabActivityHelpe
 
         if (BuildConfig.FLAVOR.equals("obf")) {
             result.removeItem(ITEM_ALERT);
+            result.removeItem(ITEM_ADDITIVES);
             result.updateName(ITEM_OBF, new StringHolder(getString(R.string.open_food_drawer)));
         }
 
@@ -418,6 +419,7 @@ public class MainActivity extends BaseActivity implements CustomTabActivityHelpe
 
         if (BuildConfig.FLAVOR.equals("opf")) {
             result.removeItem(ITEM_ALERT);
+            result.removeItem(ITEM_ADDITIVES);
             result.removeItem(ITEM_ADVANCED_SEARCH);
             result.removeItem(ITEM_CATEGORIES);
             result.updateName(ITEM_OBF, new StringHolder(getString(R.string.open_food_drawer)));
