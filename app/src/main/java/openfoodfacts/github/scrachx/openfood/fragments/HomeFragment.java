9e810e895cd672bb29bee0732c11f474e1937322
jaindiv26 <divyanshjain36@gmail.com>
2018-09-19 23:26:46 +0530

    Update : Removed unnecessary imports.

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/HomeFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/HomeFragment.java
@@ -1,11 +1,9 @@
 package openfoodfacts.github.scrachx.openfood.fragments;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -18,17 +16,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.io.IOException;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 import okhttp3.ResponseBody;
-import okhttp3.internal.Util;
 import openfoodfacts.github.scrachx.openfood.R;
 import openfoodfacts.github.scrachx.openfood.models.Search;
 import openfoodfacts.github.scrachx.openfood.network.OpenFoodAPIClient;
