d0e12601190ccbf49a116b4b0920b3fdc7d0f4ba
Huzaifa Iftikhar <huzaifa3196@gmail.com>
2018-09-24 01:32:19 +0530

    Use POST request for uploading/editing products

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/network/OpenFoodAPIService.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/network/OpenFoodAPIService.java
@@ -15,6 +15,7 @@ import openfoodfacts.github.scrachx.openfood.models.TagsWrapper;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
@@ -42,10 +43,11 @@ public interface OpenFoodAPIService {
     @GET("api/v0/product/{barcode}.json?fields=image_small_url,vitamins_tags,minerals_tags,amino_acids_tags,other_nutritional_substances_tags,image_front_url,image_ingredients_url,image_nutrition_url,url,code,traces_tags,ingredients_that_may_be_from_palm_oil_tags,additives_tags,allergens_hierarchy,manufacturing_places,nutriments,ingredients_from_palm_oil_tags,brands_tags,traces,categories_tags,ingredients_text,product_name,generic_name,ingredients_from_or_that_may_be_from_palm_oil_n,serving_size,allergens,origins,stores,nutrition_grade_fr,nutrient_levels,countries,countries_tags,brands,packaging,labels_tags,labels_hierarchy,cities_tags,quantity,ingredients_from_palm_oil_n,image_url,link,emb_codes_tags,states_tags,creator,created_t,last_modified_t,last_modified_by,editors_tags,nova_groups,lang,purchase_places,nutrition_data_per,no_nutrition_data")
     Single<State> getFullProductByBarcodeSingle(@Path("barcode") String barcode);
 
-    @GET("cgi/product_jqm2.pl")
-    Single<State> saveProductSingle(@Query("code") String code,
-                                    @QueryMap Map<String, String> parameters,
-                                    @Query("comment") String comment);
+    @FormUrlEncoded
+    @POST("cgi/product_jqm2.pl")
+    Single<State> saveProductSingle(@Field("code") String code,
+                                    @FieldMap Map<String, String> parameters,
+                                    @Field("comment") String comment);
 
     @GET("api/v0/product/{barcode}.json?fields=image_small_url,product_name,brands,quantity,image_url,nutrition_grade_fr,code")
     Call<State> getShortProductByBarcode(@Path("barcode") String barcode);
