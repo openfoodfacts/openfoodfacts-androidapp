c49828e1c9be5020f9e7be066dee1bb65ffa327b
dobriseb <35341017+dobriseb@users.noreply.github.com>
2019-01-03 15:32:59 +0100

    Operational ingredient import
    First operationnal ingredient import commit.

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/splash/SplashPresenter.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/splash/SplashPresenter.java
@@ -49,17 +49,58 @@ public class SplashPresenter implements ISplashPresenter.Actions {
                         productRepository.getLabels(true),
                         productRepository.getTags(true),
                         productRepository.getAllergens(true),
+                        productRepository.getIngredients(false),
                         productRepository.getCountries(true),
                         productRepository.getAdditives(true),
-                        productRepository.getCategories(true), (labels, tags, allergens, countries, additives, categories) -> {
+                        productRepository.getCategories(true), (labels, tags, allergens, ingredients, countries, additives, categories) -> {
                             Completable.merge(
                                     Arrays.asList(
                                             Completable.fromAction(() -> productRepository.saveLabels(labels)),
                                             Completable.fromAction(() -> productRepository.saveTags(tags)),
                                             Completable.fromAction(() -> productRepository.saveAllergens(allergens)),
+                                            Completable.fromAction(() -> productRepository.saveIngredients(ingredients)),
                                             Completable.fromAction(() -> productRepository.saveCountries(countries)),
                                             Completable.fromAction(() -> productRepository.saveAdditives(additives)),
                                             Completable.fromAction(() -> productRepository.saveCategories(categories))
+                                    )
+                            ).subscribeOn(Schedulers.computation())
+                                    .subscribe(() -> {
+                                        settings.edit().putLong(Utils.LAST_REFRESH_DATE, System.currentTimeMillis()).apply();
+                                    }, Throwable::printStackTrace);
+
+                            return true;
+                        })
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .toCompletable()
+                        //.doOnSubscribe(d -> view.showLoading())
+                        .subscribe(() -> {
+                            //view.hideLoading(false);
+                            view.navigateToMainActivity();
+                        }, e -> {
+                            e.printStackTrace();
+                            //view.hideLoading(true);
+                            view.navigateToMainActivity();
+                        });
+            } else {
+                view.navigateToMainActivity();
+            }
+        } else if (BuildConfig.FLAVOR.equals("obf")) {
+            boolean firstRun = settings.getBoolean("firstRun", true);
+            if (firstRun) {
+                settings.edit()
+                        .putBoolean("firstRun", false)
+                        .apply();
+            }
+
+            if (isNeedToRefresh()) { //true if data was refreshed more than 1 day ago
+                Single.zip(
+                        productRepository.getIngredients(false),
+                        productRepository.getCountries(true), (ingredients, countries) -> {
+                            Completable.merge(
+                                    Arrays.asList(
+                                            Completable.fromAction(() -> productRepository.saveIngredients(ingredients)),
+                                            Completable.fromAction(() -> productRepository.saveCountries(countries))
                                     )
                             ).subscribeOn(Schedulers.computation())
                                     .subscribe(() -> {
