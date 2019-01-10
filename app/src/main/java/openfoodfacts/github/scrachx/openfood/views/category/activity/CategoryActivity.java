b1956663efaec9121c6b998b46378496d79e443e
Pierre Slamich <teolemon@users.noreply.github.com>
2019-01-10 13:37:16 +0100

    Update CategoryActivity.java

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/category/activity/CategoryActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/category/activity/CategoryActivity.java
@@ -64,7 +64,7 @@ public class CategoryActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
 
-                String url = "https://fr.openfoodfacts.org/hunger-game";
+                String url = "https://world.openfoodfacts.org/hunger-game";
                 CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
                 CustomTabsIntent customTabsIntent = builder.build();
                 customTabsIntent.launchUrl(getBaseContext(), Uri.parse(url));
