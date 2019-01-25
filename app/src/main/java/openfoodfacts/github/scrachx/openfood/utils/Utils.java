12092fd8827a06bfdcb01b5d0be29294b90982b4
subhanjansaha <subhanjan.saha1998@gmail.com>
2019-01-25 21:31:21 +0530

    Fixes #2142: Do not display a question mark on product page if the Nutri-Score is not available

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/utils/Utils.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/utils/Utils.java
@@ -242,10 +242,10 @@ public class Utils {
     }
 
     public static int getImageGrade(String grade) {
-        int drawable;
+        int drawable = 0;
 
         if (grade == null) {
-            return R.drawable.ic_help_outline_orange_24dp;
+            return drawable;
         }
 
         switch (grade.toLowerCase(Locale.getDefault())) {
@@ -264,19 +264,16 @@ public class Utils {
             case "e":
                 drawable = R.drawable.nnc_e;
                 break;
-            default:
-                drawable = R.drawable.ic_help_outline_orange_24dp;
-                break;
         }
 
         return drawable;
     }
 
     public static int getNovaGroupDrawable(String novaGroup) {
-        int drawable;
+        int drawable = 0;
 
         if (novaGroup == null) {
-            return R.drawable.ic_help_outline_orange_24dp;
+            return drawable;
         }
 
         switch (novaGroup) {
@@ -291,9 +288,6 @@ public class Utils {
                 break;
             case "4":
                 drawable = R.drawable.ic_nova_group_4;
-                break;
-            default:
-                drawable = R.drawable.ic_help_outline_orange_24dp;
                 break;
         }
         return drawable;
--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/product/nutrition/NutritionProductFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/product/nutrition/NutritionProductFragment.java
@@ -203,7 +203,11 @@ public class NutritionProductFragment extends BaseFragment implements CustomTabA
                                                     salt.getImageLevel()));
             }
 
-            img.setImageDrawable(ContextCompat.getDrawable(context, Utils.getImageGrade(product.getNutritionGradeFr())));
+            if (Utils.getImageGrade(product.getNutritionGradeFr()) != 0) {
+                img.setImageDrawable(ContextCompat.getDrawable(context, Utils.getImageGrade(product.getNutritionGradeFr())));
+            } else {
+                img.setVisibility(View.INVISIBLE);
+            }
             img.setOnClickListener(view1 -> {
                 CustomTabsIntent customTabsIntent = CustomTabsHelper.getCustomTabsIntent(getContext(), customTabActivityHelper.getSession());
 
