c9c634d8a27798dca5d267c4fa442790855ffb2e
Subhanjan Saha <f20170713@pilani.bits-pilani.ac.in>
2019-02-15 17:59:18 +0530

    Fixes #2071: Moving recycling instructions to the environnement tab (#2224)
    * Fixes #2071: Moving recycling instructions to the environnement tab
    
    * Added condition to hide the carbon footprint cardview

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/product/environment/EnvironmentProductFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/product/environment/EnvironmentProductFragment.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +28,16 @@ public class EnvironmentProductFragment extends BaseFragment {
     TextView carbonFootprint;
     @BindView(R.id.environment_info_text)
     TextView environmentInfoText;
+    @BindView(R.id.recyclingInstructionToDiscard)
+    TextView recyclingInstructionToDiscardText;
+    @BindView(R.id.recyclingInstructionToRecycle)
+    TextView recyclingInstructionToRecycleText;
+    @BindView(R.id.recycling_instructions_discard_cv)
+    CardView recyclingInstructionsToDiscardCv;
+    @BindView(R.id.recycling_instructions_recycle_cv)
+    CardView recyclingInstructionsToRecycleCv;
+    @BindView(R.id.carbon_footprint_cv)
+    CardView carbonFootprintCardView;
 
     private State mState;
 
@@ -52,6 +63,8 @@ public class EnvironmentProductFragment extends BaseFragment {
             carbonFootprint.setText(bold(getString(R.string.textCarbonFootprint)));
             carbonFootprint.append(carbonFootprintNutriment.getFor100g());
             carbonFootprint.append(carbonFootprintNutriment.getUnit());
+        } else {
+            carbonFootprintCardView.setVisibility(View.GONE);
         }
 
         if (product.getEnvironmentInfocard() != null && !product.getEnvironmentInfocard().isEmpty()) {
@@ -60,6 +73,20 @@ public class EnvironmentProductFragment extends BaseFragment {
             } else {
                 environmentInfoText.append(Html.fromHtml(product.getEnvironmentInfocard()));
             }
+        }
+
+        if(product.getRecyclingInstructionsToDiscard() != null && !product.getRecyclingInstructionsToDiscard().isEmpty()) {
+            recyclingInstructionToDiscardText.setText(bold("Recycling instructions - To discard: "));
+            recyclingInstructionToDiscardText.append(product.getRecyclingInstructionsToDiscard());
+        } else {
+            recyclingInstructionsToDiscardCv.setVisibility(View.GONE);
+        }
+
+        if (product.getRecyclingInstructionsToRecycle() != null && !product.getRecyclingInstructionsToRecycle().isEmpty()) {
+            recyclingInstructionToRecycleText.setText(bold("Recycling instructions - To recycle:"));
+            recyclingInstructionToRecycleText.append(product.getRecyclingInstructionsToRecycle());
+        } else {
+            recyclingInstructionsToRecycleCv.setVisibility(View.GONE);
         }
 
         refreshView(mState);
