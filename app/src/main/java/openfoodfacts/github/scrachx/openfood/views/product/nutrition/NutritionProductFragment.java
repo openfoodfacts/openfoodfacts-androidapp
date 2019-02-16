e38fd6b072573e513d455b4f36f6830134a62b18
Balaji R <balajiramavathu12@gmail.com>
2019-02-16 17:23:26 +0530

    Fixes #247: Add link to show more info about traffic lights (high, lo… (#2127)
    * Fixes #247: Add link to show more info about traffic lights (high, low, average)
    
    * Changed nutriscore_info text
    
    * Update app/src/main/res/values/strings.xml
    
    Co-Authored-By: balaji-ramavathu <balajiramavathu12@gmail.com>
    
    * url extracts from string resources

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/product/nutrition/NutritionProductFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/product/nutrition/NutritionProductFragment.java
@@ -19,7 +19,11 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -119,6 +123,8 @@ public class NutritionProductFragment extends BaseFragment implements CustomTabA
     TextView addPhotoLabel;
     @BindView(R.id.nutriments_recycler_view)
     RecyclerView nutrimentsRecyclerView;
+    @BindView(R.id.textNutriScoreInfo)
+    TextView textNutriScoreInfo;
     @BindView(R.id.nutrient_levels_card_view)
     CardView nutrientLevelsCardView;
 
@@ -299,6 +305,22 @@ public class NutritionProductFragment extends BaseFragment implements CustomTabA
 
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
         rv.setAdapter(new NutrientLevelListAdapter(getContext(), levelItem));
+
+        textNutriScoreInfo.setClickable(true);
+        textNutriScoreInfo.setMovementMethod(LinkMovementMethod.getInstance());
+        SpannableStringBuilder spannableStringBuilder=new SpannableStringBuilder();
+        ClickableSpan clickableSpan=new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+                customTabsIntent.intent.putExtra("android.intent.extra.REFERRER", Uri.parse("android-app://" + getActivity().getPackageName()));
+                CustomTabActivityHelper.openCustomTab(getActivity(), customTabsIntent, Uri.parse(getString(R.string.url_nutrient_values)), new WebViewFallback());
+
+            }
+        };
+        spannableStringBuilder.append(getString(R.string.txtNutriScoreInfo));
+        spannableStringBuilder.setSpan(clickableSpan,0,spannableStringBuilder.length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textNutriScoreInfo.setText(spannableStringBuilder);
 
         if (TextUtils.isEmpty(product.getServingSize())) {
             serving.setVisibility(View.GONE);
