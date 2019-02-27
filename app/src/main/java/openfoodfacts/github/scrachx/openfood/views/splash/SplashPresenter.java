1e71a70d952698c7c73f04e2979aadab687a2056
Prashant Khandelwal <f2016930@pilani.bits-pilani.ac.in>
2019-02-27 18:55:54 +0530

    fixes issue #2105: loaded taxonomies in the background (#2122)
    * fixes issue #2105: loaded taxonomies in the background
    
    fixes issue #2105: loaded taxonomies in the background
    
    * added string resources.
    
    * Apply suggestions from code review
    
    Co-Authored-By: prashantkh19 <f2016930@pilani.bits-pilani.ac.in>
    
    * Added delay in the splash activity in first run
    
    * Apply suggestions from code review
    
    Co-Authored-By: prashantkh19 <f2016930@pilani.bits-pilani.ac.in>

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/splash/SplashPresenter.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/splash/SplashPresenter.java
@@ -1,17 +1,15 @@
 package openfoodfacts.github.scrachx.openfood.views.splash;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 
-import java.util.Arrays;
-
-import io.reactivex.Completable;
-import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 import openfoodfacts.github.scrachx.openfood.BuildConfig;
-import openfoodfacts.github.scrachx.openfood.repositories.IProductRepository;
-import openfoodfacts.github.scrachx.openfood.repositories.ProductRepository;
 import openfoodfacts.github.scrachx.openfood.utils.Utils;
+import openfoodfacts.github.scrachx.openfood.views.LoadTaxonomiesService;
+
+;
 
 /**
  * Created by Lobster on 03.03.18.
@@ -26,12 +24,12 @@ public class SplashPresenter implements ISplashPresenter.Actions {
 
     private ISplashPresenter.View view;
     private SharedPreferences settings;
-    private IProductRepository productRepository;
+    Context context;
 
-    public SplashPresenter(SharedPreferences settings, ISplashPresenter.View view) {
+    public SplashPresenter(SharedPreferences settings, ISplashPresenter.View view, Context context) {
         this.view = view;
         this.settings = settings;
-        productRepository = ProductRepository.getInstance();
+        this.context = context;
     }
 
     @Override
@@ -43,43 +41,17 @@ public class SplashPresenter implements ISplashPresenter.Actions {
                         .putBoolean("firstRun", false)
                         .apply();
             }
-
             if (isNeedToRefresh()) { //true if data was refreshed more than 1 day ago
-                Single.zip(
-                        productRepository.getLabels(true),
-                        productRepository.getTags(true),
-                        productRepository.getAllergens(true),
-                        productRepository.getCountries(true),
-                        productRepository.getAdditives(true),
-                        productRepository.getCategories(true), (labels, tags, allergens, countries, additives, categories) -> {
-                            Completable.merge(
-                                    Arrays.asList(
-                                            Completable.fromAction(() -> productRepository.saveLabels(labels)),
-                                            Completable.fromAction(() -> productRepository.saveTags(tags)),
-                                            Completable.fromAction(() -> productRepository.saveAllergens(allergens)),
-                                            Completable.fromAction(() -> productRepository.saveCountries(countries)),
-                                            Completable.fromAction(() -> productRepository.saveAdditives(additives)),
-                                            Completable.fromAction(() -> productRepository.saveCategories(categories))
-                                    )
-                            ).subscribeOn(Schedulers.computation())
-                                    .subscribe(() -> {
-                                        settings.edit().putLong(Utils.LAST_REFRESH_DATE, System.currentTimeMillis()).apply();
-                                    }, Throwable::printStackTrace);
-
-                            return true;
-                        })
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .toCompletable()
-                        //.doOnSubscribe(d -> view.showLoading())
-                        .subscribe(() -> {
-                            //view.hideLoading(false);
-                            view.navigateToMainActivity();
-                        }, e -> {
-                            e.printStackTrace();
-                            //view.hideLoading(true);
-                            view.navigateToMainActivity();
-                        });
+                Intent intent = new Intent(context, LoadTaxonomiesService.class);
+                context.startService(intent);
+            }
+            if (firstRun) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        view.navigateToMainActivity();
+                    }
+                }, 6000);
             } else {
                 view.navigateToMainActivity();
             }
@@ -89,7 +61,7 @@ public class SplashPresenter implements ISplashPresenter.Actions {
     }
 
     /*
-    * This method checks if data was refreshed more than 1 day ago
+     * This method checks if data was refreshed more than 1 day ago
      */
     private Boolean isNeedToRefresh() {
         return System.currentTimeMillis() - settings.getLong(Utils.LAST_REFRESH_DATE, 0) > REFRESH_PERIOD;
