9e24dbe2fda4749e4edbb6a2f3a87891d7ed6ca1
Philippe Auriach <p.auriach@gmail.com>
2019-03-15 16:46:11 +0100

    use only flag View.SYSTEM_UI_FLAG_FULLSCREEN (#2430)

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ContinuousScanActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ContinuousScanActivity.java
@@ -536,11 +536,8 @@ public class ContinuousScanActivity extends android.support.v7.app.AppCompatActi
         View decorView = getWindow().getDecorView();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            // Hide the status bar
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
         } else {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         }
