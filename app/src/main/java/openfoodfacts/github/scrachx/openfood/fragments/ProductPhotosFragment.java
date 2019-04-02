1f7a7710f7e57ebee8cb87f87dd54e2d8d3ef186
Frederic Deniger <frederic.deniger@gmail.com>
2019-04-02 13:53:16 +0200

    Fix #2504 App crashes when going to "Product Image" tab

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/ProductPhotosFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/fragments/ProductPhotosFragment.java
@@ -7,42 +7,34 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import openfoodfacts.github.scrachx.openfood.R;
 import openfoodfacts.github.scrachx.openfood.models.Product;
-import openfoodfacts.github.scrachx.openfood.models.ProductImageField;
 import openfoodfacts.github.scrachx.openfood.models.State;
 import openfoodfacts.github.scrachx.openfood.network.OpenFoodAPIClient;
 import openfoodfacts.github.scrachx.openfood.utils.Utils;
 import openfoodfacts.github.scrachx.openfood.views.FullScreenImage;
 import openfoodfacts.github.scrachx.openfood.views.adapters.ImagesAdapter;
 import openfoodfacts.github.scrachx.openfood.views.product.ProductFragment;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 
 /**
  * @author prajwalm
  */
-
 public class ProductPhotosFragment extends BaseFragment implements ImagesAdapter.OnImageClickInterface {
-
     private OpenFoodAPIClient openFoodAPIClient;
     private Product product;
-    private ProductPhotosFragment mFragment;
     // A Array list to store image names
     private ArrayList<String> imageNames;
     private RecyclerView imagesRecycler;
     private ImagesAdapter adapter;
-    private HashMap<String, String> imgMap;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,7 +42,6 @@ public class ProductPhotosFragment extends BaseFragment implements ImagesAdapter
 
         openFoodAPIClient = new OpenFoodAPIClient(getActivity());
         return createView(inflater, container, R.layout.fragment_product_photos);
-
     }
 
     @Override
@@ -64,72 +55,56 @@ public class ProductPhotosFragment extends BaseFragment implements ImagesAdapter
             state = ProductFragment.mState;
         }
         product = state.getProduct();
-        mFragment = this;
         // initialize the arraylist
         imageNames = new ArrayList<>();
         imagesRecycler = view.findViewById(R.id.images_recycler);
-        imgMap = new HashMap<>();
 
+        openFoodAPIClient.getImages(product.getCode(), (value, response) -> {
 
-        openFoodAPIClient.getImages(product.getCode(), new OpenFoodAPIClient.OnImagesCallback() {
-            @Override
-            public void onImageResponse(boolean value, String response) {
+            if (value && response != null) {
 
-                if (value && response != null) {
+                // a json object referring to base json object
+                JSONObject jsonObject = Utils.createJsonObject(response);
 
-                    // a json object referring to base json object
-                    JSONObject jsonObject = Utils.createJsonObject(response);
-
-                    // a json object referring to images
-                    JSONObject images = null;
-                    try {
-                        images = jsonObject.getJSONObject("product").getJSONObject("images");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    if (images != null) {
+                // a json object referring to images
+                JSONObject images = null;
+                try {
+                    images = jsonObject.getJSONObject("product").getJSONObject("images");
+                } catch (JSONException e) {
+                    Log.w(ProductPhotosFragment.class.getSimpleName(), "can get product / images in jon", e);
+                }
+                if (images != null) {
+                    final JSONArray names = images.names();
+                    if (names != null) {
                         // loop through all the image names and store them in a array list
-                        for (int i = 0; i < images.names().length(); i++) {
-
+                        for (int i = 0; i < names.length(); i++) {
                             try {
                                 // do not include images with contain nutrients,ingredients or other in their names
                                 // as they are duplicate and do not load as well
-                                if (images.names().getString(i).contains("n") ||
-                                        images.names().getString(i).contains("f") ||
-                                        images.names().getString(i).contains("i") ||
-                                        images.names().getString(i).contains("o")) {
+                                final String namesString = names.getString(i);
+                                if (namesString.contains("n") ||
+                                    namesString.contains("f") ||
+                                    namesString.contains("i") ||
+                                    namesString.contains("o")) {
 
                                     continue;
-
                                 }
-                                imageNames.add(images.names().getString(i));
+                                imageNames.add(namesString);
                             } catch (JSONException e) {
-                                e.printStackTrace();
+                                Log.w(ProductPhotosFragment.class.getSimpleName(), "can get product / images in jon", e);
                             }
-
-
                         }
                     }
-
-                    //Check if user is logged in
-                    SharedPreferences preferences = getActivity().getSharedPreferences("login", 0);
-                    String login = preferences.getString("user", null);
-                    if (login != null) {
-                        adapter = new ImagesAdapter(getContext(), imageNames, product.getCode(), ProductPhotosFragment.this::onImageClick, product, true);
-                    } else {
-                        adapter = new ImagesAdapter(getContext(), imageNames, product.getCode(), ProductPhotosFragment.this::onImageClick, product, false);
-                    }
-                    imagesRecycler.setAdapter(adapter);
-                    imagesRecycler.setLayoutManager(new GridLayoutManager(getContext(), 3));
-
-
                 }
 
-
+                //Check if user is logged in
+                SharedPreferences preferences = getActivity().getSharedPreferences("login", 0);
+                String login = preferences.getString("user", null);
+                adapter = new ImagesAdapter(getContext(), imageNames, product.getCode(), ProductPhotosFragment.this::onImageClick, product, login != null);
+                imagesRecycler.setAdapter(adapter);
+                imagesRecycler.setLayoutManager(new GridLayoutManager(getContext(), 3));
             }
         });
-
     }
 
     public void openFullScreen(String mUrlImage) {
@@ -149,23 +124,13 @@ public class ProductPhotosFragment extends BaseFragment implements ImagesAdapter
         String barcodePattern = product.getCode();
         if (barcodePattern.length() > 8) {
             barcodePattern = new StringBuilder(product.getCode())
-                    .insert(3, "/")
-                    .insert(7, "/")
-                    .insert(11, "/")
-                    .toString();
+                .insert(3, "/")
+                .insert(7, "/")
+                .insert(11, "/")
+                .toString();
         }
-
-        String finalUrlString = baseUrlString + barcodePattern + "/" + imageNames.get(position) + ".jpg";
-
-        imgMap.put("imgid", imageNames.get(position));
-        imgMap.put("id", ProductImageField.OTHER.toString() + '_' + product.getLang());
-
-
-        openFullScreen(finalUrlString);
-
+        openFullScreen(baseUrlString + barcodePattern + "/" + imageNames.get(position) + ".jpg");
     }
-
-
 }
 
 
