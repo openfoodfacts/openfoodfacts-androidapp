0f0b1e6c1a885f21bdcc05990cf01fbafd05c075
deniger <47569605+deniger@users.noreply.github.com>
2019-04-29 10:20:35 +0200

    Fix #2574: Can't install both OpenFoodFacts and OpenBeautyFacts (#2587)

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/utils/SearchSuggestionProvider.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/utils/SearchSuggestionProvider.java
@@ -1,9 +1,10 @@
 package openfoodfacts.github.scrachx.openfood.utils;
 
 import android.content.SearchRecentSuggestionsProvider;
+import openfoodfacts.github.scrachx.openfood.BuildConfig;
 
 public class SearchSuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "openfoodfacts.github.scrachx.openfood.utils.SearchSuggestionProvider";
+    public final static String AUTHORITY = BuildConfig.APPLICATION_ID+".utils.SearchSuggestionProvider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public SearchSuggestionProvider() {
