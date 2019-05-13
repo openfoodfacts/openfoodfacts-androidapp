3513a717b5a76c16125b0651aa58f71883414469
Frederic Deniger <frederic.deniger@gmail.com>
2019-05-13 16:33:50 +0200

    Try to fix #2625

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/customtabs/WebViewFallback.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/customtabs/WebViewFallback.java
@@ -3,6 +3,7 @@ package openfoodfacts.github.scrachx.openfood.views.customtabs;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 
 /**
  * Fallback of customtab with a standard web view
@@ -12,6 +13,10 @@ public class WebViewFallback implements CustomTabActivityHelper.CustomTabFallbac
     @Override
     public void openUri(Activity activity, Uri uri) {
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        activity.startActivity(intent);
+        try {
+            activity.startActivity(intent);
+        } catch (Exception e) {
+            Log.e(WebViewFallback.class.getSimpleName(),"can't start activity"+activity.getClass().getName(),e);
+        }
     }
 }
