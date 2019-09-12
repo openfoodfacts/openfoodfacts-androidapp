cfd76dc5759e51e3b4ca5469e72a3bc3592ea552
Frederic Deniger <frederic.deniger@gmail.com>
2019-09-12 15:45:47 +0200

    Fix #2876

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/product/summary/SummaryProductFragment.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/product/summary/SummaryProductFragment.java
@@ -611,7 +611,9 @@ public class SummaryProductFragment extends BaseFragment implements CustomTabAct
         getActivity().runOnUiThread(() -> {
             switch (state) {
                 case LOADING: {
-                    categoryProduct.append(getString(R.string.txtLoading));
+                    if(getContext()!=null) {
+                        categoryProduct.append(getString(R.string.txtLoading));
+                    }
                     break;
                 }
                 case EMPTY: {
