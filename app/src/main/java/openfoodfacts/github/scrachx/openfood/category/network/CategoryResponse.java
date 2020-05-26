fd776c7d6213f3470fc7248330c404fd0400f302
Pierre Slamich <pierre.slamich@gmail.com>
2020-05-26 23:25:36 +0200

    Apply suggestions from code review

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/category/network/CategoryResponse.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/category/network/CategoryResponse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.Collections;
 import java.util.List;
-
 /**
  * Class for response received from CategoryNetworkService class
  */
