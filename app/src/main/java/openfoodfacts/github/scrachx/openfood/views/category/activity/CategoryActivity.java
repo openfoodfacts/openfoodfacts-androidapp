33f5ca3fdc95ecb9ee9afd41588c6b7e609f3d61
VaiTon <eyadlorenzo@gmail.com>
2020-06-14 01:05:47 +0200

    fix: set fragment from code in CategoryActivity, should close https://github.com/openfoodfacts/openfoodfacts-androidapp/issues/3231

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/category/activity/CategoryActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/category/activity/CategoryActivity.java
@@ -9,15 +9,17 @@ import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.widget.Button;
 
 import androidx.browser.customtabs.CustomTabsIntent;
+
+import java.util.Objects;
 
 import openfoodfacts.github.scrachx.openfood.R;
 import openfoodfacts.github.scrachx.openfood.databinding.ActivityCategoryBinding;
 import openfoodfacts.github.scrachx.openfood.utils.ShakeDetector;
 import openfoodfacts.github.scrachx.openfood.utils.Utils;
 import openfoodfacts.github.scrachx.openfood.views.BaseActivity;
+import openfoodfacts.github.scrachx.openfood.views.category.fragment.CategoryListFragment;
 import openfoodfacts.github.scrachx.openfood.views.listeners.BottomNavigationListenerInstaller;
 
 public class CategoryActivity extends BaseActivity {
@@ -42,23 +44,28 @@ public class CategoryActivity extends BaseActivity {
 
         setSupportActionBar(binding.toolbarInclude.toolbar);
         setTitle(R.string.category_drawer);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Button gameButton = findViewById(R.id.game_button);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         SharedPreferences shakePreference = PreferenceManager.getDefaultSharedPreferences(this);
         scanOnShake = shakePreference.getBoolean("shakeScanMode", false);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mShakeDetector = new ShakeDetector();
 
-        mShakeDetector.setOnShakeListener(count -> {
-            if (scanOnShake) {
-                Utils.scan(CategoryActivity.this);
-            }
-        });
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager != null) {
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mShakeDetector = new ShakeDetector();
+
+            mShakeDetector.setOnShakeListener(count -> {
+                if (scanOnShake) {
+                    Utils.scan(CategoryActivity.this);
+                }
+            });
+        }
 
         // chrome custom tab for category hunger game
-        gameButton.setOnClickListener(v -> openHungerGame());
+        binding.gameButton.setOnClickListener(v -> openHungerGame());
+
+        // set fragment container view
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment, new CategoryListFragment()).commitNow();
 
         BottomNavigationListenerInstaller.selectNavigationItem(binding.bottomNavigationInclude.bottomNavigation, 0);
         BottomNavigationListenerInstaller.install(binding.bottomNavigationInclude.bottomNavigation, this);
