74ed97fac16fb1199647ebf81b1466d6daf31b21
VaiTon <eyadlorenzo@gmail.com>
2020-06-30 14:02:00 +0200

    fix: SplashPresenter was still using IntentService
    Closes https://github.com/openfoodfacts/openfoodfacts-androidapp/issues/3312.

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/splash/SplashPresenter.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/splash/SplashPresenter.java
@@ -1,11 +1,11 @@
 package openfoodfacts.github.scrachx.openfood.views.splash;
 
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.os.Handler;
-import android.os.ResultReceiver;
+
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 import openfoodfacts.github.scrachx.openfood.AppFlavors;
 import openfoodfacts.github.scrachx.openfood.repositories.Taxonomy;
@@ -16,14 +16,14 @@ import openfoodfacts.github.scrachx.openfood.views.LoadTaxonomiesService;
  * Created by Lobster on 03.03.18.
  */
 public class SplashPresenter implements ISplashPresenter.Actions {
-    private final Context context;
+    private final SplashActivity activity;
     private final SharedPreferences settings;
     private final ISplashPresenter.View view;
 
-    SplashPresenter(SharedPreferences settings, ISplashPresenter.View view, Context context) {
+    SplashPresenter(SharedPreferences settings, ISplashPresenter.View view, SplashActivity activity) {
         this.view = view;
         this.settings = settings;
-        this.context = context;
+        this.activity = activity;
     }
 
     private void activateDownload(Taxonomy taxonomy) {
@@ -55,21 +55,18 @@ public class SplashPresenter implements ISplashPresenter.Actions {
                 .putBoolean("firstRun", false)
                 .apply();
         }
-        final ResultReceiver receiver = new ResultReceiver(new Handler()) {
-            @Override
-            protected void onReceiveResult(int resultCode, Bundle resultData) {
-                if (resultCode == LoadTaxonomiesService.STATUS_RUNNING) {
-                    view.showLoading();
-                } else {
-                    view.hideLoading(resultCode == LoadTaxonomiesService.STATUS_ERROR);
-                }
-            }
-        };
 
         // The service will load server resources only if newer than already downloaded...
-        Intent intent = new Intent(context, LoadTaxonomiesService.class);
-        intent.putExtra(LoadTaxonomiesService.RECEIVER_KEY, receiver);
-        context.startService(intent);
+        OneTimeWorkRequest request = OneTimeWorkRequest.from(LoadTaxonomiesService.class);
+        WorkManager manager = WorkManager.getInstance(activity);
+        manager.enqueue(request);
+        manager.getWorkInfoByIdLiveData(request.getId()).observe(activity, workInfo -> {
+            if (workInfo != null && workInfo.getState() == WorkInfo.State.RUNNING) {
+                view.showLoading();
+            } else if (workInfo != null) {
+                view.hideLoading(workInfo.getState() == WorkInfo.State.FAILED);
+            }
+        });
 
         if (firstRun) {
             new Handler().postDelayed(view::navigateToMainActivity, 6000);
