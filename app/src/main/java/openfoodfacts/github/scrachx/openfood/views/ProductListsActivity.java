45cf75026f72d1512b3eacb752295054a008456b
VaiTon <eyadlorenzo@gmail.com>
2020-07-11 18:54:05 +0200

    design: added night res for ic_plus_dark_24
    design: renamed plus icons

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ProductListsActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/ProductListsActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2020 Open Food Facts
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package openfoodfacts.github.scrachx.openfood.views;
 
 import android.app.ProgressDialog;
@@ -87,7 +103,7 @@ public class ProductListsActivity extends BaseActivity implements SwipeControlle
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         BottomNavigationListenerInstaller.install(binding.bottomNavigation.bottomNavigation, this);
-        binding.fabAdd.setCompoundDrawablesWithIntrinsicBounds(R.drawable.plus_blue, 0, 0, 0);
+        binding.fabAdd.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_plus_blue_24, 0, 0, 0);
 
         productListsDao = getProductListsDaoWithDefaultList(this);
         productLists = productListsDao.loadAll();
