10f6b9902f73d2db9729ddc1ef93ea4ea3fb952a
Philippe Auriach <p.auriach@gmail.com>
2020-07-12 09:23:15 +0200

    Migrate travis « release » to github action

---
+++ fastlane/envfiles/decrypt_secret.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$API_JSON_FILE_DECRYPTKEY" \
+--output api-4712693179220384697-162836-33ea08672303.json api-4712693179220384697-162836-33ea08672303.json.gpg
