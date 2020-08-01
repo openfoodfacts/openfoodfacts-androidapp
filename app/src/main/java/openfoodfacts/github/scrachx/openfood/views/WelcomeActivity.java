fe52600c7ed3221b27f40695568f9285a4f3c21e
Pierre Slamich <pierre@openfoodfacts.org>
2020-08-01 12:14:42 +0200

    tests

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/views/WelcomeActivity.java
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/views/WelcomeActivity.java
@@ -122,8 +122,8 @@ public class WelcomeActivity extends AppCompatActivity {
 
         layouts = new int[]{
             R.layout.welcome_slide1,
-            R.layout.welcome_slide2,
             R.layout.welcome_slide3,
+            R.layout.welcome_slide2,
             R.layout.welcome_slide4
         };
 
