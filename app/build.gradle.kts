bbc2936547e5bda9307f4d37901b0a5356bf178e
VaiTon <eyadlorenzo@gmail.com>
2020-10-31 19:44:10 +0100

    build: bumped versionName to 3.5.0 and versionCode to 350

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -193,8 +193,8 @@ android {
         minSdkVersion(16)
         targetSdkVersion(29)
 
-        versionCode = 340
-        versionName = "3.4.0"
+        versionCode = 350
+        versionName = "3.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
