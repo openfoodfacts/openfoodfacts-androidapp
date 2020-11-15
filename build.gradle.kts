e43442e662f8fbeb45f58dbfb1115e46e4faaf17
VaiTon <eyadlorenzo@gmail.com>
2020-11-15 12:34:41 +0100

    build: updated android gradle plugin from 4.1.0 to 4.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ buildscript {
         maven(url = "https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("com.android.tools.build:gradle:4.1.1")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
