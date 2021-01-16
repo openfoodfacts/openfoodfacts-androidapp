f0a9c5d989b3e4f5aa8a0953001fb8ce75d1a51a
mrudultora <mrudultora@gmail.com>
2021-01-16 17:23:31 +0530

    Hided ecoscore in obf scan

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/features/scan/ContinuousScanActivity.kt
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/features/scan/ContinuousScanActivity.kt
@@ -292,9 +292,11 @@ class ContinuousScanActivity : AppCompatActivity() {
         binding.quickViewNovaGroup.visibility = View.GONE
     }
 
-    private fun quickViewCheckEcoScore(product: Product) {
+    private fun quickViewCheckEcoScore(product: Product) = if (isFlavors(AppFlavors.OFF)) {
         binding.quickViewEcoscoreIcon.setImageResource(product.getEcoscoreResource())
         binding.quickViewEcoscoreIcon.visibility = View.VISIBLE
+    } else {
+        binding.quickViewEcoscoreIcon.visibility = View.GONE
     }
 
     private fun tryDisplayOffline(
