21c2ab5be250d4b3e14ee440302bc8abe70eae1d
Pierre Slamich <pierre@openfoodfacts.org>
2021-04-29 08:58:04 +0200

    Fixes #3887

---
+++ app/src/test/java/openfoodfacts/github/scrachx/openfood/features/product/view/contributors/ContributorsFragmentTest.kt
@@ -0,0 +1,11 @@
+package openfoodfacts.github.scrachx.openfood.features.product.view.contributors
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ContributorsFragmentTest {
+    @Test
+    fun `test incomplete states`() {
+        assertThat(ContributorsFragment.isIncompleteState("images-not-selected")).isTrue()
+    }
+}
