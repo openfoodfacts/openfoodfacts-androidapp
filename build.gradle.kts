ae9135640acac6b29b1bc95573c43ac762c21e76
VaiTon <eyadlorenzo@gmail.com>
2021-05-09 13:06:10 +0200

    build: update AGP from 4.1.3 to 4.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("com.android.tools.build:gradle:4.2.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
