59a861609f22864fcd945359cd81da7ac21652f6
VaiTon <eyadlorenzo@gmail.com>
2021-07-27 18:52:29 +0200

    ux: don't open list after product addition

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/features/product/view/summary/DialogAddToListAdapter.kt
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/features/product/view/summary/DialogAddToListAdapter.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import openfoodfacts.github.scrachx.openfood.R
 import openfoodfacts.github.scrachx.openfood.features.product.view.summary.DialogAddToListAdapter.ListViewHolder
-import openfoodfacts.github.scrachx.openfood.features.productlist.ProductListActivity
 import openfoodfacts.github.scrachx.openfood.models.DaoSession
 import openfoodfacts.github.scrachx.openfood.models.entities.ListedProduct
 import openfoodfacts.github.scrachx.openfood.models.entities.ProductLists
@@ -23,30 +22,29 @@ class DialogAddToListAdapter(
         private val imageUrl: String,
         private val daoSession: DaoSession
 ) : RecyclerView.Adapter<ListViewHolder>() {
+    override fun getItemCount() = productLists.size
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.dialog_add_to_list_recycler_item, parent, false)
         return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        val listName = productLists[position].listName
-        val listId = productLists[position].id
-        holder.tvListTitle.text = listName
+        val list = productLists[position]
+
+        holder.tvListTitle.text = list.listName
         holder.itemView.setOnClickListener {
             val product = ListedProduct().also {
                 it.barcode = barcode
-                it.listId = listId
-                it.listName = listName
+                it.listId = list.id
+                it.listName = list.listName
                 it.productName = productName
                 it.productDetails = productDetails
                 it.imageUrl = imageUrl
             }
             daoSession.listedProductDao.insertOrReplace(product)
-            ProductListActivity.start(context, listId, listName)
         }
     }
-
-    override fun getItemCount() = productLists.size
 
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tvListTitle: TextView = itemView.findViewById(R.id.tvDialogListName)
