bee27ba71bea1297789abdadff724c727ebace5a
VaiTon <eyadlorenzo@gmail.com>
2022-03-01 00:25:00 +0100

    Revert "chore: fixed tests"
    This reverts commit b32d11ad13afe20d2481f3d86f2b494579bb46bf.

--- app/src/test/java/openfoodfacts/github/scrachx/openfood/images/ImageKeyHelperTest.kt
+++ app/src/test/java/openfoodfacts/github/scrachx/openfood/images/ImageKeyHelperTest.kt
@@ -1,58 +1,54 @@
 package openfoodfacts.github.scrachx.openfood.images
 
-import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth
 import openfoodfacts.github.scrachx.openfood.BuildConfig
 import openfoodfacts.github.scrachx.openfood.models.Product
-import openfoodfacts.github.scrachx.openfood.models.ProductImageField.FRONT
-import openfoodfacts.github.scrachx.openfood.models.ProductImageField.INGREDIENTS
-import openfoodfacts.github.scrachx.openfood.models.asBarcode
+import openfoodfacts.github.scrachx.openfood.models.ProductImageField
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
+import org.mockito.Mockito
 
 class ImageKeyHelperTest {
     private lateinit var mockProduct: Product
 
     @BeforeEach
     fun setUp() {
-        mockProduct = mock()
+        mockProduct = Mockito.mock(Product::class.java)
     }
 
     @Test
     fun imageStringKey_returnsCorrectString() {
-        whenever(mockProduct.lang).doReturn("de")
-        assertThat(mockProduct.getImageStringKey(FRONT)).isEqualTo("front_de")
+        Mockito.`when`(mockProduct.lang).thenReturn("de")
+        Truth.assertThat(mockProduct.getImageStringKey(ProductImageField.FRONT)).isEqualTo("front_de")
     }
 
     @Test
     fun languageCodeFromUrl_blankURL() {
         val url = ""
-        assertThat(getLanguageCodeFromUrl(INGREDIENTS, url)).isNull()
+        Truth.assertThat(getLanguageCodeFromUrl(ProductImageField.INGREDIENTS, url)).isNull()
     }
 
     @Test
     fun languageCodeFromUrl_returnsCorrectLanguage() {
         val url = "https://static.openfoodfacts.org/images/products/541/004/100/1204/ingredients_de.48.100.jpg"
-        assertThat(getLanguageCodeFromUrl(INGREDIENTS, url)).isEqualTo("de")
+        Truth.assertThat(getLanguageCodeFromUrl(ProductImageField.INGREDIENTS, url)).isEqualTo("de")
     }
 
     @Test
     fun imageUrl_BarcodeShorter() {
-        val barcode = "303371".asBarcode()
+        val barcode = "303371"
         val imageName = "Image"
         val size = "big"
         val expected = BuildConfig.STATICURL + "/images/products/303371/Imagebig.jpg"
-        assertThat(getImageUrl(barcode, imageName, size)).isEqualTo(expected)
+        Truth.assertThat(getImageUrl(barcode, imageName, size)).isEqualTo(expected)
     }
 
     @Test
     fun imageUrl_BarcodeLonger() {
-        val barcode = "3033710001279".asBarcode()
+        val barcode = "3033710001279"
         val imageName = "Image"
         val size = "big"
         val expected = BuildConfig.STATICURL + "/images/products/303/371/000/1279/Imagebig.jpg"
-        assertThat(getImageUrl(barcode, imageName, size)).isEqualTo(expected)
+        Truth.assertThat(getImageUrl(barcode, imageName, size)).isEqualTo(expected)
     }
 }
