ca432baa56685737524fd56e8d0e7e887c43ed8c
VaiTon <eyadlorenzo@gmail.com>
2022-03-03 22:30:50 +0100

    chore(hilt): convert PhotoReceiverHandler to hilt service
    refactor: use helper method to create Intent in kotlin style

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/features/compare/ProductCompareActivity.kt
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/features/compare/ProductCompareActivity.kt
@@ -32,12 +32,17 @@ import openfoodfacts.github.scrachx.openfood.listeners.CommonBottomListenerInsta
 import openfoodfacts.github.scrachx.openfood.models.Product
 import openfoodfacts.github.scrachx.openfood.models.ProductImageField
 import openfoodfacts.github.scrachx.openfood.repositories.ProductRepository
+import openfoodfacts.github.scrachx.openfood.utils.Intent
 import openfoodfacts.github.scrachx.openfood.utils.PhotoReceiverHandler
 import openfoodfacts.github.scrachx.openfood.utils.isHardwareCameraInstalled
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class ProductCompareActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityProductComparisonBinding
+
+    private val viewModel: ProductCompareViewModel by viewModels()
 
     @Inject
     lateinit var client: ProductRepository
@@ -51,10 +56,8 @@ class ProductCompareActivity : BaseActivity() {
     @Inject
     lateinit var productViewActivityStarter: ProductViewActivityStarter
 
-    private lateinit var binding: ActivityProductComparisonBinding
-    private lateinit var photoReceiverHandler: PhotoReceiverHandler
-
-    private val viewModel: ProductCompareViewModel by viewModels()
+    @Inject
+    lateinit var photoReceiverHandler: PhotoReceiverHandler
 
     private val scanProductContract = registerForActivityResult(SimpleScanActivityContract()) { barcode ->
         barcode?.let { viewModel.barcodeDetected(it) }
@@ -75,36 +78,35 @@ class ProductCompareActivity : BaseActivity() {
         }
 
         lifecycleScope.launch {
-            viewModel.sideEffectFlow
-                .flowWithLifecycle(lifecycle)
-                .collect {
-                    when (it) {
-                        is SideEffect.ProductAlreadyAdded -> showProductAlreadyAddedDialog()
-                        is SideEffect.ProductNotFound -> showProductNotFoundDialog()
-                        is SideEffect.ConnectionError -> showConnectionErrorDialog()
+            launch {
+                viewModel.sideEffectFlow
+                    .flowWithLifecycle(lifecycle)
+                    .collect {
+                        when (it) {
+                            is SideEffect.ProductAlreadyAdded -> showProductAlreadyAddedDialog()
+                            is SideEffect.ProductNotFound -> showProductNotFoundDialog()
+                            is SideEffect.ConnectionError -> showConnectionErrorDialog()
+                        }
                     }
-                }
-        }
-        lifecycleScope.launch {
-            viewModel.productsFlow
-                .flowWithLifecycle(lifecycle)
-                .distinctUntilChanged()
-                .collect { products ->
-                    createAdapter(products)
-                }
-        }
-        lifecycleScope.launch {
-            viewModel.loadingVisibleFlow
-                .flowWithLifecycle(lifecycle)
-                .distinctUntilChanged()
-                .collect {
-                    binding.comparisonProgressView.isVisible = it
-                }
+            }
+            launch {
+                viewModel.productsFlow
+                    .flowWithLifecycle(lifecycle)
+                    .distinctUntilChanged()
+                    .collect(::createAdapter)
+            }
+            launch {
+                viewModel.loadingVisibleFlow
+                    .flowWithLifecycle(lifecycle)
+                    .distinctUntilChanged()
+                    .collect {
+                        binding.comparisonProgressView.isVisible = it
+                    }
+            }
         }
 
-        binding.productComparisonButton.setOnClickListener {
-            openScanActivity()
-        }
+
+        binding.productComparisonButton.setOnClickListener { openScanActivity() }
     }
 
     override fun startScanActivity() {
@@ -119,7 +121,9 @@ class ProductCompareActivity : BaseActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        photoReceiverHandler.onActivityResult(this, requestCode, resultCode, data)
+        photoReceiverHandler.onActivityResult(this, requestCode, resultCode, data) {
+            (binding.productComparisonRv.adapter as ProductCompareAdapter).onImageReturned(it)
+        }
     }
 
     private fun createAdapter(products: List<ProductCompareViewModel.CompareProduct>) {
@@ -155,10 +159,6 @@ class ProductCompareActivity : BaseActivity() {
         binding.productComparisonRv.layoutManager = LinearLayoutManager(this@ProductCompareActivity, HORIZONTAL, false)
         binding.productComparisonRv.adapter = productComparisonAdapter
 
-        // Set activity result handler
-        photoReceiverHandler = PhotoReceiverHandler(sharedPreferences) {
-            productComparisonAdapter.onImageReturned(it)
-        }
     }
 
     private fun openScanActivity() {
@@ -215,13 +215,10 @@ class ProductCompareActivity : BaseActivity() {
         const val KEY_PRODUCTS_TO_COMPARE = "products_to_compare"
 
         @JvmStatic
-        fun start(context: Context, product: Product) {
-            context.startActivity(Intent(context, ProductCompareActivity::class.java).apply {
-                putExtra(KEY_PRODUCTS_TO_COMPARE, product)
+        fun start(context: Context, product: Product? = null) {
+            context.startActivity(Intent<ProductCompareActivity>(context) {
+                if (product != null) putExtra(KEY_PRODUCTS_TO_COMPARE, product)
             })
         }
-
-        @JvmStatic
-        fun start(context: Context) = context.startActivity(Intent(context, ProductCompareActivity::class.java))
     }
 }
