1fdcee170f755f505ae723bd817209fac3fbaf8a
Edouard Marquez <g123k@users.noreply.github.com>
2022-04-09 08:37:42 +0200

    Fix requested changes

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/analytics/AnalyticsTrackingEvent.kt
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/analytics/AnalyticsTrackingEvent.kt
@@ -2,21 +2,18 @@ package openfoodfacts.github.scrachx.openfood.analytics
 
 import android.app.Activity
 import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.milliseconds
 
-fun Activity.startTrackEvent(event : AnalyticsEvent): AnalyticsTrackingEvent {
-    return AnalyticsTrackingEvent(event)
+fun Activity.startTrackEvent(event: AnalyticsEvent): AnalyticsTrackingEvent {
+    return AnalyticsTrackingEvent(event = event)
 }
 
 data class AnalyticsTrackingEvent(
-    val startDate: Long,
+    val startDate: Long = System.currentTimeMillis(),
     val event: AnalyticsEvent
 ) {
 
-    constructor(event: AnalyticsEvent) : this(System.currentTimeMillis(), event)
-
-    fun computeDurationInSeconds() =
-        TimeUnit.MILLISECONDS.toSeconds(
-            System.currentTimeMillis() - startDate
-        ).toFloat()
+    fun computeDurationInSeconds(): Float =
+        (System.currentTimeMillis() - startDate).milliseconds.inWholeSeconds.toFloat()
 
 }
