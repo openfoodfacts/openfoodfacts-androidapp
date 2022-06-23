fcff9118a99c1f36f0f91c547ee28874d2623071
Christopher Hatton <christopherhattonuk@gmail.com>
2022-06-23 23:19:57 +1000

    Adopt Gradle Version Catalog feature

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 include(":app")
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
