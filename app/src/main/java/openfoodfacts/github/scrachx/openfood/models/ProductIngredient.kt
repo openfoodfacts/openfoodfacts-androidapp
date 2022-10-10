589f9532fe9f12f1823875bf92e85564696ea5a1
VaiTon <eyadlorenzo@gmail.com>
2022-10-10 11:49:29 +0200

    chore: remove toString implementation from data class

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/models/ProductIngredient.kt
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/models/ProductIngredient.kt
@@ -1,21 +1,24 @@
 package openfoodfacts.github.scrachx.openfood.models
 
-import com.fasterxml.jackson.annotation.*
-import org.apache.commons.lang3.builder.ToStringBuilder
-import org.apache.commons.lang3.builder.ToStringStyle
+import com.fasterxml.jackson.annotation.JsonAnyGetter
+import com.fasterxml.jackson.annotation.JsonAnySetter
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import java.io.Serializable
-import java.util.*
 
+/**
+ * Represents an ingredient of the product
+ *
+ * @param rank The rank, set -1 if no rank returned
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder("text", "id", "rank", "percent")
 data class ProductIngredient(
-        @JsonProperty("text") val text: String,
-        @JsonProperty("id") val id: String,
-        /**
-         * The rank, set -1 if no rank returned
-         */
-        @JsonProperty("rank") val rank: Long = 0,
-        @JsonProperty("percent") val percent: String? = null
+    @JsonProperty("text") val text: String,
+    @JsonProperty("id") val id: String,
+    @JsonProperty("rank") val rank: Long = 0,
+    @JsonProperty("percent") val percent: String? = null,
 ) : Serializable {
 
 
@@ -26,14 +29,6 @@ data class ProductIngredient(
     fun setAdditionalProperty(name: String, value: Any) {
         additionalProperties[name] = value
     }
-
-    override fun toString() = ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
-            .append("text", text)
-            .append("id", id)
-            .append("rank", rank)
-            .append("percent", percent)
-            .append("additionalProperties", additionalProperties)
-            .toString()
 
     companion object {
         private const val serialVersionUID = 1L
