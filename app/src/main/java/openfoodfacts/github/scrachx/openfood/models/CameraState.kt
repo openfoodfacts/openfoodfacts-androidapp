a115b1268c9c70e03bd5612d2625dbfa40ac3e90
VaiTon <eyadlorenzo@gmail.com>
2022-10-10 13:37:05 +0200

    chore: in CameraState use value property in fromInt

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/models/CameraState.kt
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/models/CameraState.kt
@@ -4,10 +4,7 @@ enum class CameraState(val value: Int) {
     Back(0), Front(1);
 
     companion object {
-        fun fromInt(value: Int) = when (value) {
-            0 -> Back
-            1 -> Front
-            else -> throw IllegalStateException("Unsupported camera state $value.")
-        }
+        fun fromInt(value: Int): CameraState =
+            values().first { it.value == value }
     }
 }
