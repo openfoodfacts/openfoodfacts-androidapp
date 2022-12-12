4da60b108d2003a6c0bd1fc5bebc49a30af5979c
VaiTon <eyadlorenzo@gmail.com>
2022-12-12 22:12:26 +0100

    chore: use androidx version of getSystemService in NetworkConnectivityRepository

--- app/src/main/java/openfoodfacts/github/scrachx/openfood/repositories/NetworkConnectivityRepository.kt
+++ app/src/main/java/openfoodfacts/github/scrachx/openfood/repositories/NetworkConnectivityRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
+import androidx.core.content.getSystemService
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -17,13 +18,13 @@ class NetworkConnectivityRepository @Inject constructor(
      * Check if the user is connected to a network. This can be any network.
      * @return `true` if connected or connecting, `false` otherwise
      */
-    @Suppress("DEPRECATION")
     fun isNetworkAvailable(): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val cm = context.getSystemService<ConnectivityManager>() ?: return false
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val capability = cm.getNetworkCapabilities(cm.activeNetwork)
             capability?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
         } else {
+            @Suppress("DEPRECATION")
             cm.activeNetworkInfo?.isConnectedOrConnecting ?: false
         }
     }
